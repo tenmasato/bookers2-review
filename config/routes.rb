085f59b663ae95e940e5770e24e5f8810be471eb
tenmasato <narito1015@icloud.com>
2021-11-06 11:39:37 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :books,only: [:new,:create,:index,:show,:destroy,:edit,:update]do
     resource :favorites,only:[:create,:destroy]
+    resources :book_comments,only:[:create,:destroy]
   end
   resources :users,only: [:show,:index,:edit,:update]
 end
